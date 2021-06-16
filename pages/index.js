dab8eaf4266632e7be1d71ff0351fd4599ad50d3
NoomRT <rueangyotnoom@gmail.com>
2021-06-16 17:28:27 +0700

    Linking Between Pages

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
@@ -11,6 +11,7 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
       <Footer />
     </div>
   )
