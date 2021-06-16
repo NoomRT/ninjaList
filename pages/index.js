edc888117c2f248a72296a788951a2ba4c5798fb
NoomRT <rueangyotnoom@gmail.com>
2021-06-16 18:26:34 +0700

    Images & Metadata

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
     <div>
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
@@ -14,5 +19,6 @@ export default function Home() {
         <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
+    </>
   )
 }
